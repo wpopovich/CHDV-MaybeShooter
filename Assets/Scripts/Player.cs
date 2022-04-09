75005a8287f909679b0c25ea18889c8cd2ffe363
mateoburella <mateoburella03@gmail.com>
2022-04-09 20:40:27 -0300

    Animacion de player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,29 +6,54 @@ public class Player : MonoBehaviour
 {
     public float speed;
     public float runningSpeed;
+    float actualSpeed;
     new private Rigidbody rigidbody;
+    public Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        float actualSpeed = speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move();
+        Movement();
     }
 
-    void Move()
+    void Movement()
     {
+        // Sneaking
 
-        float actualSpeed = speed;
-        if((Input.GetAxisRaw("Run") > 0)) {
-            Debug.Log("Running");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0, vertical) * actualSpeed;
+        rigidbody.MovePosition((transform.position + movement * Time.deltaTime));
+
+        // Running
+
+        if (Input.GetKeyDown(KeyCode.LeftShift)) // Player está corriendo
+        {
             actualSpeed = runningSpeed;
         }
-        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")) * actualSpeed;
-        rigidbody.MovePosition(transform.position + movement * Time.deltaTime);
+        else
+        {
+             actualSpeed = speed;
+        }
+
+        // Animator
+
+        if(movement != Vector3.zero) // Player se está moviendo
+        {
+            animator.SetBool("IsSneaking", true);
+        }
+
+        else // Player no se está moviendo
+        {
+            animator.SetBool("IsSneaking", false);
+        }
     }
 }
