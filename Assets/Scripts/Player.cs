7a94d94819732d67f18247171499185b32556891
mateoburella <mateoburella03@gmail.com>
2022-04-10 11:25:05 -0300

    Fix de movimiento del player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,47 +6,71 @@ public class Player : MonoBehaviour
 {
     public float speed;
     public float runningSpeed;
-    float actualSpeed;
+    float currentSpeed;
+    public float maxStamina = 100;
+    float currentStamina;
     new private Rigidbody rigidbody;
     public Animator animator;
+    bool isRunning;
 
-    // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
-        float actualSpeed = speed;
+        float currentSpeed = speed;
+        currentStamina = maxStamina;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate() // FixedUpdate para evitar que el personaje traspase las paredes
     {
         Movement();
+        Debug.Log(currentStamina);
     }
 
     void Movement()
     {
-        // Sneaking
+
+        // Movimiento 
 
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
-        Vector3 movement = new Vector3(horizontal, 0, vertical) * actualSpeed;
-        rigidbody.MovePosition((transform.position + movement * Time.deltaTime));
+        Vector3 movement = new Vector3(horizontal, 0, vertical) * currentSpeed;
+        
 
-        // Running
-
-        if (Input.GetKeyDown(KeyCode.LeftShift)) // Player está corriendo
+        if(horizontal != 0 && vertical != 0)
         {
-            actualSpeed = runningSpeed;
+            rigidbody.MovePosition(transform.position + movement * Time.deltaTime * 0.707f); // Para evitar que el personaje se mueva más rapido en las diagonales
         }
         else
         {
-             actualSpeed = speed;
+            rigidbody.MovePosition(transform.position + movement * Time.deltaTime);
+        }
+
+        // Running
+
+        if (Input.GetKey(KeyCode.LeftShift) && movement != Vector3.zero && currentStamina > 0) // Player está corriendo
+        {
+            Run();
+            
+        }
+        else // Player no está corriendo
+        {
+            Walk();
+        }
+
+        if (movement == Vector3.zero && currentStamina != maxStamina) // Si el player está quieto, regenera su stamina
+        {
+            RegenerarStamina();
+        }
+
+        if (currentStamina > maxStamina)
+        {
+            currentStamina = maxStamina;
         }
 
         // Animator
 
-        if(movement != Vector3.zero) // Player se está moviendo
+        if (movement != Vector3.zero) // Player se está moviendo
         {
             animator.SetBool("IsSneaking", true);
         }
@@ -55,5 +79,37 @@ public class Player : MonoBehaviour
         {
             animator.SetBool("IsSneaking", false);
         }
+
+        if (isRunning == true)
+        {
+            animator.SetBool("IsRunning", true);
+        }
+
+        else
+        {
+            animator.SetBool("IsRunning", false);
+        }
     }
+
+    #region Métodos
+
+    void Run()
+    {
+        currentSpeed = runningSpeed;
+        currentStamina -= 1f;
+        isRunning = true;
+    }
+
+    void Walk()
+    {
+        currentSpeed = speed;
+        isRunning = false;
+    }
+
+    void RegenerarStamina()
+    {
+        currentStamina += 0.6f;
+    }
+
+    #endregion
 }
